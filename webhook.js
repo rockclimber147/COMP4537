e92c8083ab50bcd1945f246bb8951428634c14b8
Daylen Smith <daylencsmith@gmail.com>
2025-09-14 14:42:37 -0700

    add simple server listener hook

---
+++ webhook.js
@@ -0,0 +1,34 @@
+import express from "express";
+import crypto from "crypto";
+import { exec } from "child_process";
+
+const app = express();
+app.use(express.json());
+
+const secret = "mysecret123";
+
+function verifySignature(req) {
+  const signature = req.headers['x-hub-signature-256'];
+  const hmac = crypto.createHmac('sha256', secret);
+  const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  return signature === digest;
+}
+
+app.post("/git-webhook", (req, res) => {
+  if (!verifySignature(req)) {
+    res.status(403).send("Forbidden");
+    return;
+  }
+
+  exec("cd /var/www/COMP4537 && git pull origin main", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Pull failed");
+      return;
+    }
+    console.log(stdout);
+    res.status(200).send("Pull successful");
+  });
+});
+
+app.listen(3001, () => console.log("Webhook listener running on port 3001"));
