e70ea29b7b0b0e8b41bb3cf3cb0a2e17f594865b
rockclimber147 <daylencsmith@gmail.com>
2025-09-23 13:10:58 -0700

    add file count protection

--- labs/3/modules/utils.js
+++ labs/3/modules/utils.js
@@ -9,8 +9,19 @@ class DateTimeHelper {
 }
 
 class FileHelper {
+    static BASE_DIR_NAME = "files"
+    static MAX_FILES = 5
+
+    constructor() {
+        this.BASE_DIR = path.resolve(FileHelper.BASE_DIR_NAME);
+
+        if (!fs.existsSync(this.BASE_DIR)) {
+            fs.mkdirSync(this.BASE_DIR, { recursive: true });
+        }
+    }
+
     readFile(fileName) {
-        const filePath = path.resolve(fileName);
+        const filePath = path.join(this.BASE_DIR, fileName);
         try {
             const data = fs.readFileSync(filePath, "utf-8");
             return data;
@@ -21,13 +32,35 @@ class FileHelper {
     }
 
     writeFile(content, fileName = "file.txt") {
-        const filePath = path.resolve(fileName);
+        if (!this.fileExists(fileName) && !this.canAddFile()) return false
+        const filePath = path.join(this.BASE_DIR, fileName);
         try {
             fs.appendFileSync(filePath, content, "utf-8");
             console.log(`File ${fileName} appended successfully.`);
             return true;
         } catch (err) {
             console.error(`Error writing file ${fileName}:`, err.message);
+            return false;
+        }
+    }
+
+    fileExists(fileName) {
+        const filePath = path.join(this.BASE_DIR, fileName);
+        try {
+            return fs.existsSync(filePath) && fs.statSync(filePath).isFile();
+        } catch (err) {
+            console.error(`Error checking file ${fileName}:`, err.message);
+            return false;
+        }
+    }
+
+    canAddFile() {
+        try {
+            const files = fs.readdirSync(this.BASE_DIR, { withFileTypes: true });
+            const fileCount = files.filter(f => f.isFile()).length;
+            return fileCount < FileHelper.MAX_FILES;
+        } catch (err) {
+            console.error("Error checking files in base directory:", err.message);
             return false;
         }
     }
