ae5ba47c36e111b664fc6db7d583fca58e853167
rockclimber147 <daylencsmith@gmail.com>
2025-09-23 12:07:22 -0700

    add initial file logic

--- labs/3/modules/utils.js
+++ labs/3/modules/utils.js
@@ -1,10 +1,39 @@
-const dateTimeHelper = {
-    getDate: function () {
+const fs = require("fs");
+const path = require("path");
+
+class DateTimeHelper {
+    getDate() {
         const now = new Date();
         return now.toISOString();
     }
-};
+}
+
+class FileHelper {
+    readFile(fileName) {
+        const filePath = path.resolve(fileName);
+        try {
+            const data = fs.readFileSync(filePath, "utf-8");
+            return data;
+        } catch (err) {
+            console.error(`Error reading file ${fileName}:`, err.message);
+            return null;
+        }
+    }
+
+    writeFile(content, fileName="file.txt") {
+        const filePath = path.resolve(fileName);
+        try {
+            fs.writeFileSync(filePath, content, "utf-8");
+            console.log(`File ${fileName} written successfully.`);
+            return true;
+        } catch (err) {
+            console.error(`Error writing file ${fileName}:`, err.message);
+            return false;
+        }
+    }
+}
 
 module.exports = {
-    dateTime: dateTimeHelper
+    dateTime: new DateTimeHelper(),
+    fileHelper: new FileHelper(),
 };
--- labs/3/server.js
+++ labs/3/server.js
@@ -3,17 +3,39 @@ const url = require("url")
 const PORT = 3001;
 
 const Messages = require("./lang/en/en")
-const dateUtils = require("./modules/utils")
-const dateHelper = dateUtils.dateTime
+const Utils = require("./modules/utils")
+const dateHelper = Utils.dateTime
+const fileHelper = Utils.fileHelper
+
+class Endpoints {
+    static GET_DATE = "/getDate"
+    static READ_FILE = "/readFile"
+    static WRITE_FILE = "/writeFile/"
+}
+
 const routes = {
-    "GET": {
-        "/getDate": (req, res, query) => {
+    GET: {
+        [Endpoints.GET_DATE]: (req, res, query) => {
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(`${Messages.WELCOME.replace(`%1`, query["name"])} ${dateHelper.getDate()}`);
+            res.end(`${Messages.WELCOME.replace("%1", query["name"])} ${dateHelper.getDate()}`);
+        },
+        [Endpoints.READ_FILE]: (req, res, fileName) => {
+            const contents = fileHelper.readFile(fileName);
+            if (contents === null) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end(`File "${fileName}" not found`);
+            } else {
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.end(contents);
+            }
         },
     },
-    "POST": {
-
+    POST: {
+        [Endpoints.WRITE_FILE]: (req, res, query) => {
+            const text = query["text"]
+            fileHelper.writeFile(text)
+            res.end()
+        },
     }
 };
 
@@ -21,6 +43,12 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
     const query = parsedUrl.query;
+
+    if (pathname.startsWith(`/readFile`) && req.method === "GET") {
+        const fileName = decodeURIComponent(pathname.replace(`/readFile/`, ""));
+        routes.GET[Endpoints.READ_FILE](req, res, fileName);
+        return;
+    }
 
     const handler = routes[req.method]?.[pathname];
     if (handler) {
